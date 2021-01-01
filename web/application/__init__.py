d1730255c5026d46287c68466900c1d0d872c1e7
lalarustamli <rustamlilala@gmail.com>
2021-01-02 01:49:09 +0200

    config

--- web/application/__init__.py
+++ web/application/__init__.py
@@ -1,5 +1,13 @@
+from .config import Config
 from flask import Flask
+from flask_mongoengine import MongoEngine
+
 
 app = Flask(__name__)
+
+app.config.from_object(Config)
+
+db = MongoEngine()
+db.init_app(app)
 
 from . import routes
